c89f1670dcbf88035fb8b54d19df5a770b7aa767
nosemasashi1013 <nose_masashi@cyberagent.co.jp>
2021-10-07 20:00:19 +0900

    fix

--- liff.js
+++ liff.js
@@ -33,24 +33,11 @@ async function sendMessages(text) {
   liff.closeWindow();
 }
 
-function drawLine(begin, end, color) {
-  canvas.beginPath();
-  canvas.moveTo(begin.x, begin.y);
-  canvas.lineTo(end.x, end.y);
-  canvas.lineWidth = 4;
-  canvas.strokeStyle = color;
-  canvas.stroke();
-}
-
 $(function () {
   // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
   // LINE DevelopersのLIFF画面より確認可能
   const liffId = "1656505610-JBZmjw3A";
   initializeLiff(liffId);
-
-  $(".open-camera-btn").click(function () {
-    scanCode();
-  });
 
   $(".submit-btn").click(function () {
     const date = $('input[type="date"]').val();
@@ -62,72 +49,89 @@ $(function () {
     return false;
   });
 
-  const video = document.createElement("video");
-  const canvasElement = document.getElementById("canvas");
-  const canvas = canvasElement.getContext("2d");
-  const loadingMessage = document.getElementById("loadingMessage");
-  const outputContainer = document.getElementById("output");
-  const outputMessage = document.getElementById("outputMessage");
-  const outputData = document.getElementById("outputData");
+  $(".open-camera-btn").click(function () {
+    // scanCode();
+    const video = document.createElement("video");
+    const canvasElement = document.getElementById("canvas");
+    const canvas = canvasElement.getContext("2d");
+    const loadingMessage = document.getElementById("loadingMessage");
+    const outputContainer = document.getElementById("output");
+    const outputMessage = document.getElementById("outputMessage");
+    const outputData = document.getElementById("outputData");
 
-  // Use facingMode: environment to attemt to get the front camera on phones
-  navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function (stream) {
-      video.srcObject = stream;
-      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-      video.play();
-      requestAnimationFrame(tick);
-    });
-
-  function tick() {
-    loadingMessage.innerText = "⌛ Loading video...";
-    if (video.readyState === video.HAVE_ENOUGH_DATA) {
-      loadingMessage.hidden = true;
-      canvasElement.hidden = false;
-      outputContainer.hidden = false;
-
-      canvasElement.height = video.videoHeight;
-      canvasElement.width = video.videoWidth;
-      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-      var imageData = canvas.getImageData(
-        0,
-        0,
-        canvasElement.width,
-        canvasElement.height
-      );
-      var code = jsQR(imageData.data, imageData.width, imageData.height, {
-        inversionAttempts: "dontInvert",
-      });
-      if (code) {
-        drawLine(
-          code.location.topLeftCorner,
-          code.location.topRightCorner,
-          "#FF3B58"
-        );
-        drawLine(
-          code.location.topRightCorner,
-          code.location.bottomRightCorner,
-          "#FF3B58"
-        );
-        drawLine(
-          code.location.bottomRightCorner,
-          code.location.bottomLeftCorner,
-          "#FF3B58"
-        );
-        drawLine(
-          code.location.bottomLeftCorner,
-          code.location.topLeftCorner,
-          "#FF3B58"
-        );
-        outputMessage.hidden = true;
-        outputData.parentElement.hidden = false;
-        outputData.innerText = code.data;
-      } else {
-        outputMessage.hidden = false;
-        outputData.parentElement.hidden = true;
-      }
+    function drawLine(begin, end, color) {
+      canvas.beginPath();
+      canvas.moveTo(begin.x, begin.y);
+      canvas.lineTo(end.x, end.y);
+      canvas.lineWidth = 4;
+      canvas.strokeStyle = color;
+      canvas.stroke();
     }
-    requestAnimationFrame(tick);
-  }
+    // Use facingMode: environment to attemt to get the front camera on phones
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: "environment" } })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+        requestAnimationFrame(tick);
+      });
+
+    function tick() {
+      loadingMessage.innerText = "⌛ Loading video...";
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        loadingMessage.hidden = true;
+        canvasElement.hidden = false;
+        outputContainer.hidden = false;
+
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(
+          video,
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        var imageData = canvas.getImageData(
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+          inversionAttempts: "dontInvert",
+        });
+        if (code) {
+          drawLine(
+            code.location.topLeftCorner,
+            code.location.topRightCorner,
+            "#FF3B58"
+          );
+          drawLine(
+            code.location.topRightCorner,
+            code.location.bottomRightCorner,
+            "#FF3B58"
+          );
+          drawLine(
+            code.location.bottomRightCorner,
+            code.location.bottomLeftCorner,
+            "#FF3B58"
+          );
+          drawLine(
+            code.location.bottomLeftCorner,
+            code.location.topLeftCorner,
+            "#FF3B58"
+          );
+          outputMessage.hidden = true;
+          outputData.parentElement.hidden = false;
+          outputData.innerText = code.data;
+        } else {
+          outputMessage.hidden = false;
+          outputData.parentElement.hidden = true;
+        }
+      }
+      requestAnimationFrame(tick);
+    }
+  });
 });
